f1878b9d7c858071e6520baab5088e4305848332
joaopaulo <joaopaulorubio@gmail.com>
2016-04-14 14:52:14 -0300

    as

--- DemoWS/src/br/jp/Connection/ConnectionFactoryBD.java
+++ DemoWS/src/br/jp/Connection/ConnectionFactoryBD.java
@@ -1,10 +1,8 @@
 package br.jp.Connection;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 public class ConnectionFactoryBD {
 //?autoReconnect=true&useSSL=false
